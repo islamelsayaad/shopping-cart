bb39517f24b9c0a8554367b8bee6b7fa48a2ffff
Islam Elsayaad <imsayaad@gmail.com>
2024-07-26 02:48:04 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Auth0ProviderWithHistory from "./context/Auth0ProviderWithHistory.tsx";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithHistory>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        {" "}
         <App />
-      </Auth0ProviderWithHistory>
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
