f3087cf63641ea84afa71bcb188d73a95072ace9
Islam Elsayaad <81169249+islamelsayaad@users.noreply.github.com>
2024-08-03 14:24:35 +0300

    commit

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -28,8 +28,8 @@ function Cart() {
         <h3 className="mb-10 text-2xl font-medium sm:text-4xl">Your Cart</h3>
       </div>
       {cartItemsNum === 0 ? (
-        <div className="w-full h-full mt-24 text-xl text-gray-400 mb-36">
-          Your cart is empty 🌵
+        <div className="w-fit h-full mt-24 text-2xl mb-36">
+           🌵
         </div>
       ) : (
         <div className="flex flex-col w-full h-full divide-y mb-14">
