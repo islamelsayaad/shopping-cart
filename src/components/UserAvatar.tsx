bb1a20e4188ea4de6f969e6b688e4aeff8b9a835
Islam Elsayaad <81169249+islamelsayaad@users.noreply.github.com>
2024-08-03 14:29:20 +0300

    commit

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,14 +1,12 @@
-const UserAvatar = ({ user, logout }: { user: any; logout: () => void }) => {
+const UserAvatar = ({ user }: { user: any) => {
   return user?.picture !== undefined ? (
     <div
-      onClick={() => logout()}
       className="w-10 h-10 overflow-hidden rounded-full cursor-pointer"
     >
       <img className="object-contain" src={user.picture} alt="avatar" />
     </div>
   ) : (
     <button
-      onClick={() => logout()}
       className="flex items-center justify-center w-10 h-10 overflow-hidden text-lg font-medium text-center text-white bg-green-500 rounded-full cursor-pointer sm:text-xl"
     >
       {user?.name !== undefined ? user.name[0].toUpperCase() : null}
